13dfda1008ff62f0dd851eea88f567e2f8b3ff0f
Henrik Solgaard <henrik.solgaard@gmail.com>
2025-04-07 22:06:11 +0200

    Rename drizzle-forecast to cloud-peeker

---
+++ cloud-peeker-icons.el
@@ -0,0 +1,151 @@
+;;; cloud-peeker-icons.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2025 Henrik Solgaard
+;;
+;; Author: Henrik Solgaard <henrik.solgaard@gmail.com>
+;; Maintainer: Henrik Solgaard <henrik.solgaard@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Console based weather icons
+;;
+;;; Code:
+
+(defconst cloud-peeker--color-codes
+  '((black . 30)
+    (red . 31)
+    (green . 32)
+    (yellow . 33)
+    (blue . 34)
+    (magenta . 35)
+    (cyan . 36)
+    (white . 37)
+    (reset . 0)))
+
+(defun cloud-peeker-sun ()
+  "Sun."
+  '(yellow "O" reset))
+
+(defun cloud-peeker--moon ()
+  "Moon."
+  '(yellow ")" reset))
+
+(defun cloud-peeker--polartwilight ()
+  "Sun below horizon."
+  '(yellow "0" reset))
+
+(defun cloud-peeker--clear (body)
+  "BODY with no clouds."
+  `(" " ,body " "))
+
+(defun cloud-peeker--cloudy (&optional body)
+  "Cloud around optional BODY."
+  (if body
+      `("c" ,body "ɔ")
+    '("cOɔ")))
+
+(defun cloud-peeker--fair (body)
+  "Small clouds around BODY."
+  `(" " ,body "ɔ"))
+
+(defun cloud-peeker--dry ()
+  "No precipitation."
+  '())
+
+(defun cloud-peeker--fog ()
+  "Fog."
+  '("==="))
+
+(defun cloud-peeker--lightning ()
+  "Lightning."
+  '(yellow "ϟ" reset))
+
+(defconst cloud-peeker--weather-symbols-list
+  `(("clearsky_day" . (,(cloud-peeker--clear (cloud-peeker-sun)) ,(cloud-peeker--dry)))
+    ("clearsky_night" . (,(cloud-peeker--clear (cloud-peeker--moon)) ,(cloud-peeker--dry)))
+    ("clearsky_polartwilight" . (,(cloud-peeker--clear (cloud-peeker--polartwilight)) ,(cloud-peeker--dry)))
+    ("cloudy" . (,(cloud-peeker--cloudy) ,(cloud-peeker--dry)))
+    ("fair_day" . (,(cloud-peeker--fair (cloud-peeker-sun)) ,(cloud-peeker--dry)))
+    ("fair_night" . (,(cloud-peeker--fair (cloud-peeker--moon)) ,(cloud-peeker--dry)))
+    ("fair_polartwilight" . (,(cloud-peeker--fair (cloud-peeker--polartwilight)) ,(cloud-peeker--dry)))
+    ("fog" . (,(cloud-peeker--cloudy) ,(cloud-peeker--fog)))
+    ("heavyrain" . (,(cloud-peeker--cloudy) ("///")))
+    ("heavyrainandthunder" . (,(cloud-peeker--cloudy) ("/" ,(cloud-peeker--lightning) "/")))
+    ("heavyrainshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("///")))
+    ("heavyrainshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("///")))
+    ("heavyrainshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("///")))
+    ("heavyrainshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("/" ,(cloud-peeker--lightning) "/")))
+    ("heavyrainshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("/" ,(cloud-peeker--lightning) "/")))
+    ("heavyrainshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("/" ,(cloud-peeker--lightning) "/")))
+    ("heavysleet" . (,(cloud-peeker--cloudy) ("/*/")))
+    ("heavysleetandthunder" . (,(cloud-peeker--cloudy) ("/" ,(cloud-peeker--lightning) "*")))
+    ("heavysleetshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("/*/")))
+    ("heavysleetshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("/*/")))
+    ("heavysleetshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("/*/")))
+    ("heavysleetshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("/" ,(cloud-peeker--lightning) "*")))
+    ("heavysleetshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("/" ,(cloud-peeker--lightning) "*")))
+    ("heavysleetshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("/" ,(cloud-peeker--lightning) "*")))
+    ("heavysnow" . (,(cloud-peeker--cloudy) ("***")))
+    ("heavysnowandthunder" . (,(cloud-peeker--cloudy) ("*" ,(cloud-peeker--lightning) "*")))
+    ("heavysnowshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("***")))
+    ("heavysnowshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("***")))
+    ("heavysnowshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("***")))
+    ("heavysnowshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("*" ,(cloud-peeker--lightning) "*")))
+    ("heavysnowshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("*" ,(cloud-peeker--lightning) "*")))
+    ("heavysnowshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("*" ,(cloud-peeker--lightning) "*")))
+    ("lightrain" . (,(cloud-peeker--cloudy) ("' ,")))
+    ("lightrainandthunder" . (,(cloud-peeker--cloudy) ("'" ,(cloud-peeker--lightning) ",")))
+    ("lightrainshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("' ,")))
+    ("lightrainshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("' ,")))
+    ("lightrainshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("' ,")))
+    ("lightrainshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("'" ,(cloud-peeker--lightning) ",")))
+    ("lightrainshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("'" ,(cloud-peeker--lightning) ",")))
+    ("lightrainshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("'" ,(cloud-peeker--lightning) ",")))
+    ("lightsleet" . (,(cloud-peeker--cloudy) ("* ,")))
+    ("lightsleetandthunder" . (,(cloud-peeker--cloudy) ("*" ,(cloud-peeker--lightning) ",")))
+    ("lightsleetshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("* ,")))
+    ("lightsleetshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("* ,")))
+    ("lightsleetshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("* ,")))
+    ("lightsnow" . (,(cloud-peeker--cloudy) ("* *")))
+    ("lightsnowandthunder" . (,(cloud-peeker--cloudy) (" " ,(cloud-peeker--lightning) "*")))
+    ("lightsnowshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) (" * ")))
+    ("lightsnowshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) (" * ")))
+    ("lightsnowshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) (" * ")))
+    ("lightssleetshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("*" ,(cloud-peeker--lightning) ",")))
+    ("lightssleetshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("*" ,(cloud-peeker--lightning) ",")))
+    ("lightssleetshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("*" ,(cloud-peeker--lightning) ",")))
+    ("lightssnowshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) (" " ,(cloud-peeker--lightning) "*")))
+    ("lightssnowshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) (" " ,(cloud-peeker--lightning) "*")))
+    ("lightssnowshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) (" " ,(cloud-peeker--lightning) "*")))
+    ("partlycloudy_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ,(cloud-peeker--dry)))
+    ("partlycloudy_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ,(cloud-peeker--dry)))
+    ("partlycloudy_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ,(cloud-peeker--dry)))
+    ("rain" . (,(cloud-peeker--cloudy) ("/ /")))
+    ("rainandthunder" . (,(cloud-peeker--cloudy) (" " ,(cloud-peeker--lightning) "/")))
+    ("rainshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("/ /")))
+    ("rainshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("/ /")))
+    ("rainshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("/ /")))
+    ("rainshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("'" ,(cloud-peeker--lightning) "/")))
+    ("rainshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("'" ,(cloud-peeker--lightning) "/")))
+    ("rainshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("'" ,(cloud-peeker--lightning) "/")))
+    ("sleet" . (,(cloud-peeker--cloudy) ("* /")))
+    ("sleetandthunder" . (,(cloud-peeker--cloudy) ("*" ,(cloud-peeker--lightning) "/")))
+    ("sleetshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("* /")))
+    ("sleetshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("* /")))
+    ("sleetshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("* /")))
+    ("sleetshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("*" ,(cloud-peeker--lightning) "/")))
+    ("sleetshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("*" ,(cloud-peeker--lightning) "/")))
+    ("sleetshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("*" ,(cloud-peeker--lightning) "/")))
+    ("snow" . (,(cloud-peeker--cloudy) ("* *")))
+    ("snowandthunder" . (,(cloud-peeker--cloudy) (" " ,(cloud-peeker--lightning) "*")))
+    ("snowshowers_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) ("* *")))
+    ("snowshowers_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) ("* *")))
+    ("snowshowers_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) ("* *")))
+    ("snowshowersandthunder_day" . (,(cloud-peeker--cloudy (cloud-peeker-sun)) (" " ,(cloud-peeker--lightning) "*")))
+    ("snowshowersandthunder_night" . (,(cloud-peeker--cloudy (cloud-peeker--moon)) (" " ,(cloud-peeker--lightning) "*")))
+    ("snowshowersandthunder_polartwilight" . (,(cloud-peeker--cloudy (cloud-peeker--polartwilight)) (" " ,(cloud-peeker--lightning) "*")))))
+
+(provide 'cloud-peeker-icons)
+;;; cloud-peeker-icons.el ends here
--- cloud-peeker.el
+++ cloud-peeker.el
@@ -1,4 +1,4 @@
-;;; drizzle-forecast.el --- Description -*- lexical-binding: t; -*-
+;;; cloud-peeker.el --- Description -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2025 Henrik Solgaard
 ;;
@@ -7,7 +7,7 @@
 ;; Created: March 28, 2025
 ;; Version: 0.0.1
 ;; Keywords: weather
-;; Homepage: https://github.com/hsolg/emacs-drizzle-forecast
+;; Homepage: https://github.com/hsolg/emacs-cloud-peeker
 ;; Package-Requires: ((emacs "27.1"))
 ;;
 ;; This file is not part of GNU Emacs.
@@ -22,31 +22,31 @@
 (require 'url)
 (require 'iso8601)
 (require 'persist)
-(require 'drizzle-forecast-icons)
+(require 'cloud-peeker-icons)
 
-(persist-defvar drizzle-forecast-selected-location nil "Selected location (lat, lon)")
+(persist-defvar cloud-peeker-selected-location nil "Selected location (lat, lon)")
 
-(defun drizzle-forecast--translate-symbol (symbol)
+(defun cloud-peeker--translate-symbol (symbol)
   "Translate SYMBOL to escape sequence."
-  (cond ((symbolp symbol) (format "\x1b[%dm" (cdr (assoc symbol drizzle-forecast--color-codes))))
+  (cond ((symbolp symbol) (format "\x1b[%dm" (cdr (assoc symbol cloud-peeker--color-codes))))
         (t symbol)))
 
-(defun drizzle-forecast--map-symbols (symbols)
+(defun cloud-peeker--map-symbols (symbols)
   "Map SYMBOLS to output strings."
-  (mapcar #'drizzle-forecast--translate-symbol symbols))
+  (mapcar #'cloud-peeker--translate-symbol symbols))
 
-(defvar drizzle-forecast--base-dir
+(defvar cloud-peeker--base-dir
   (file-name-directory (file-truename load-file-name))
-  "Base directory of the drizzle-forecast package.")
+  "Base directory of the cloud-peeker package.")
 
-(define-derived-mode drizzle-forecast-mode special-mode "Drizzle forecast"
+(define-derived-mode cloud-peeker-mode special-mode "Cloud peeker"
   "Major mode for displaying weather forecasts.")
 
-(defun drizzle-forecast--search-locations (string)
+(defun cloud-peeker--search-locations (string)
   "Search for location names that contain STRING with the GeoNames API."
   (let* ((url-request-method "GET")
          (url (format "http://api.geonames.org/search?q=%s&type=json&style=full&isNameRequired=true&username=emacs_cloud_peeker" string))
-         (url-request-extra-headers '(("Et-Client-Name" . "emacs-drizzle-forecast")))
+         (url-request-extra-headers '(("Et-Client-Name" . "emacs-cloud-peeker")))
          (buffer (url-retrieve-synchronously url)))
     (when buffer
       (with-current-buffer buffer
@@ -57,9 +57,9 @@
           ;; (message (assoc 'geonames (json-parse-string json-text :object-type 'alist)))
           (json-parse-string json-text :object-type 'alist))))))
 
-(defun drizzle-forecast--get-locations (string)
+(defun cloud-peeker--get-locations (string)
   "Get location with name that contain STRING."
-  (let* ((res (drizzle-forecast--search-locations string))
+  (let* ((res (cloud-peeker--search-locations string))
          (geonames (cdr (assoc 'geonames res)))
          (locations (seq-map (lambda (item) (let* ((lat (cdr (assoc 'lat item)))
                                                    (lon (cdr (assoc 'lng item)))
@@ -77,12 +77,12 @@
                              geonames)))
     locations))
 
-(defun drizzle-forecast--fetch-location-forecast (latitude longitude)
+(defun cloud-peeker--fetch-location-forecast (latitude longitude)
   "Fetch weather forecast for a given LATITUDE and LONGITUDE from met.no API."
   (message (format "%s %s" latitude longitude))
   (let* ((rounded-latitude (round-coordinate latitude))
          (rounded-longitude (round-coordinate longitude))
-         (url-request-extra-headers '(("User-Agent" . "emacs-drizzle-forecast")))
+         (url-request-extra-headers '(("User-Agent" . "emacs-cloud-peeker")))
          (url (format "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=%s&lon=%s"
                       rounded-latitude rounded-longitude))
          (buffer (url-retrieve-synchronously url)))
@@ -93,9 +93,9 @@
         (let ((json-object-type 'alist))
           (json-read))))))
 
-(defun drizzle-forecast--image-for-symbol (symbol)
+(defun cloud-peeker--image-for-symbol (symbol)
   "Get image for SYMBOL."
-  (let* ((filename (format "%s/icons/weather/%s.svg" drizzle-forecast--base-dir symbol)))
+  (let* ((filename (format "%s/icons/weather/%s.svg" cloud-peeker--base-dir symbol)))
     (create-image filename 'svg nil :height 35)))
 
 (defun flatten (lst)
@@ -112,57 +112,57 @@
          (rounded (/ (float (round (* num 10000))) 10000.0)))
     (format "%.4f" rounded)))
 
-(defun drizzle-forecast--glyphs-for-symbol (symbol)
+(defun cloud-peeker--glyphs-for-symbol (symbol)
   "Get glyphs for SYMBOL."
-  (let* ((codes (cdr (assoc symbol drizzle-forecast--weather-symbols-list))))
+  (let* ((codes (cdr (assoc symbol cloud-peeker--weather-symbols-list))))
     (if codes
         (let ((sky (flatten (nth 0 codes)))
               (air (flatten (nth 1 codes))))
-          `(,(apply #'concat (drizzle-forecast--map-symbols sky))
-            ,(apply #'concat (drizzle-forecast--map-symbols air))))
+          `(,(apply #'concat (cloud-peeker--map-symbols sky))
+            ,(apply #'concat (cloud-peeker--map-symbols air))))
       '(symbol ""))))
 
-(defun drizzle-forecast--format-time (utc-string)
+(defun cloud-peeker--format-time (utc-string)
   "Format UTC-STRING as local time."
   (let* ((utc-time (iso8601-parse utc-string)))
     (format-time-string "%H:%M:%S %Z" (apply 'encode-time utc-time))))
 
-(defun drizzle-forecast--format-date (utc-string)
+(defun cloud-peeker--format-date (utc-string)
   "Format UTC-STRING as local date."
   (let* ((utc-time (iso8601-parse utc-string)))
     (format-time-string "%A, %d %B" (apply 'encode-time utc-time))))
 
-(defun drizzle-forecast--display-all-icons ()
+(defun cloud-peeker--display-all-icons ()
   "Display all console weather icons."
-  (dolist (pair drizzle-forecast--weather-symbols-list)
+  (dolist (pair cloud-peeker--weather-symbols-list)
     (let* ((name (car pair))
-           (glyphs (drizzle-forecast--glyphs-for-symbol name)))
+           (glyphs (cloud-peeker--glyphs-for-symbol name)))
       (insert (format "%s\n" name))
       (insert (format "%s\n" (nth 0 glyphs)))
       (insert (format "%s\n\n" (nth 1 glyphs)))))
   (ansi-color-apply-on-region (point-min) (point-max)))
 
-(defun drizzle-forecast--select-location (choices)
+(defun cloud-peeker--select-location (choices)
   "Select location from CHOICES."
   (let* ((selection (completing-read "Select location: " choices nil t)))
     (cdr (assoc selection choices))))
 
-(defun drizzle-forecast--prompt-location ()
+(defun cloud-peeker--prompt-location ()
   "Prompt the user for a location."
   (let* ((user-input (read-string "Location name: "))
-         (choices (drizzle-forecast--get-locations user-input))
-         (selected-location-coordinates (drizzle-forecast--select-location choices)))
+         (choices (cloud-peeker--get-locations user-input))
+         (selected-location-coordinates (cloud-peeker--select-location choices)))
     selected-location-coordinates))
 
-(defun drizzle-forecast-show-forecast (arg)
+(defun cloud-peeker-show-forecast (arg)
   "Show forecast for a selected location."
   (interactive "p")
   (when (/= arg 1)
     (message "Select location"))
 
-  (let* ((location (if (or (/= arg 1) (not drizzle-forecast-selected-location))
-                       (drizzle-forecast--prompt-location)
-                     drizzle-forecast-selected-location))
+  (let* ((location (if (or (/= arg 1) (not cloud-peeker-selected-location))
+                       (cloud-peeker--prompt-location)
+                     cloud-peeker-selected-location))
          (latitude (cdr (assoc 'lat location)))
          (longitude (cdr (assoc 'lon location)))
          (name (cdr (assoc 'name location)))
@@ -170,11 +170,11 @@
          (county (cdr (assoc 'county location)))
          (country (cdr (assoc 'country location)))
          (buffer-name "*Weather forecast*")
-         (forecast (drizzle-forecast--fetch-location-forecast latitude longitude)))
-    (setq drizzle-forecast-selected-location location)
-    (persist-save 'drizzle-forecast-selected-location)
+         (forecast (cloud-peeker--fetch-location-forecast latitude longitude)))
+    (setq cloud-peeker-selected-location location)
+    (persist-save 'cloud-peeker-selected-location)
     (with-current-buffer-window buffer-name nil nil
-      (drizzle-forecast-mode)
+      (cloud-peeker-mode)
       (let ((inhibit-read-only t))
         (erase-buffer)
         (let* ((timeseries (alist-get 'timeseries (alist-get 'properties forecast)))
@@ -191,7 +191,7 @@
                    (next-6-hour-summary (alist-get 'summary (alist-get 'next_6_hours data)))
                    (air-temperature (alist-get 'air_temperature instant-details))
                    (symbol (or (alist-get 'symbol_code next-1-hour-summary) (alist-get 'symbol_code next-6-hour-summary)))
-                   (dn (drizzle-forecast--format-date time)))
+                   (dn (cloud-peeker--format-date time)))
               (when symbol (if (display-graphic-p)
                                (progn
                                  (when (not (string= dn date-name))
@@ -199,9 +199,9 @@
                                      (insert "\n"))
                                    (insert (propertize (format "%s\n" dn) 'face '(:height 1.5)))
                                    (setq date-name dn))
-                                 (insert (propertize (format "%s    " (drizzle-forecast--format-time (format "%s" time))) 'display '(raise -0.3)))
+                                 (insert (propertize (format "%s    " (cloud-peeker--format-time (format "%s" time))) 'display '(raise -0.3)))
                                  (when symbol
-                                   (insert-image (drizzle-forecast--image-for-symbol (format "%s" symbol))))
+                                   (insert-image (cloud-peeker--image-for-symbol (format "%s" symbol))))
                                  (insert (propertize (format "    %3d °C\n" (round air-temperature)) 'display '(raise -0.3))))
                              (progn
                                (when (not (string= dn date-name))
@@ -209,8 +209,8 @@
                                    (insert "\n"))
                                  (insert (format "%s\n" dn))
                                  (setq date-name dn))
-                               (insert (format "%s    " (drizzle-forecast--format-time (format "%s" time))))
-                               (let* ((glyphs (when symbol (drizzle-forecast--glyphs-for-symbol (format "%s" symbol))))
+                               (insert (format "%s    " (cloud-peeker--format-time (format "%s" time))))
+                               (let* ((glyphs (when symbol (cloud-peeker--glyphs-for-symbol (format "%s" symbol))))
                                       (upper (when glyphs (nth 0 glyphs)))
                                       (lower (when glyphs (nth 1 glyphs)))
                                       (pos (current-column)))
@@ -229,5 +229,5 @@
           (goto-char (point-min)))))
     (pop-to-buffer buffer-name)))
 
-(provide 'drizzle-forecast)
-;;; drizzle-forecast.el ends here
+(provide 'cloud-peeker)
+;;; cloud-peeker.el ends here
